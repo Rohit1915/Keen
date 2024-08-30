29fb701ed768d17c617eeda91999de8560c6d0f1
rohit <rohit.x.kumar1@fosteringlinux.com>
2024-08-30 12:33:24 +0530

    sd

--- src/app/Components/Main.tsx
+++ src/app/Components/Main.tsx
@@ -7,22 +7,22 @@ function Main() {
       <div className="">
         <img className="" src="fotor-ai-2024082818012.jpg" alt="" />
       </div>
-      <div className=" text-black ">
-      <h1 className=" mx-40 text-3xl mt-44 mb-10 " style={{fontFamily:'fantasy'}}>
+      <div className=" text-black flex flex-col h-full my-20 mx-10 md:h-96 md:my-40 ">
+      <h1 className=" text-3xl md:mx-40 " style={{fontFamily:'fantasy'}}>
             
             MORE ABOUT KEEN AND ABLE
             </h1> 
-            <span className="mx-40 text-2xl" style={{fontFamily:'fantasy'}} >
+            <span className="md:mx-40 text-2xl mt-8" style={{fontFamily:'fantasy'}} >
             Our Story
             </span>
-            <div className="mx-40" style={{fontFamily:'fantasy'}}>
+            <div className="md:mx-40" style={{fontFamily:'fantasy'}}>
             <p className="mt-10">
             Keen and Able started as a college dream in the mind of a few youngsters who just wished to have something to call their own. Open Source happened by chance. But has stuck for long. Over the years, we have dabbled in many associated Open Source technologies and successfully helped our customers adopt them.
             </p> 
             <p className="mt-8">
             We believe our strength lies in taking challenges head on and be of help to our customers in all areas of software deployment - architecture and planning, deployment and support. 
             </p> 
-            <p className="mt8">
+            <p className="my-8">
             Being a services focused organisation, we are able to think beyond product and strive to provide our customers with solutions that help them address their business need rather than deploy a product.   
             </p>
             <p className="mt-8">
