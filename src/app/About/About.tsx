7114b21df1b3cdb72db7be3c86944f582e348e7a
rohit <rohit.x.kumar1@fosteringlinux.com>
2024-08-31 16:31:14 +0530

    about

--- src/app/About/About.tsx
+++ src/app/About/About.tsx
@@ -2,31 +2,28 @@ import React from 'react'
 
 function Main() {
   return (
-    <div className=" bg-[#d38d8d]">
+    <div className=" bg-[#282b29]">
       <div className=" grid-cols-1 mx-auto grid lg:grid-cols-2">
       <div className="">
-        <img className="" src="fotor-ai-2024082818012.jpg" alt="" />
+        <img className=" h-full" src="earth-3537401.jpg" alt="" />
       </div>
-      <div className=" text-black flex flex-col h-full my-20 mx-10 md:h-96 md:my-40 ">
+      <div className=" text-white flex flex-col h-full my-20 mx-10 md:h-96 md:my-40 ">
       <h1 className=" text-3xl md:mx-40 " style={{fontFamily:'fantasy'}}>
             
-            MORE ABOUT KEEN AND ABLE
+            OUR BACKGROUND
             </h1> 
             <span className="md:mx-40 text-2xl mt-8" style={{fontFamily:'fantasy'}} >
-            Our Story
+            How We Got Here
             </span>
             <div className="md:mx-40" style={{fontFamily:'fantasy'}}>
             <p className="mt-10">
-            Keen and Able started as a college dream in the mind of a few youngsters who just wished to have something to call their own. Open Source happened by chance. But has stuck for long. Over the years, we have dabbled in many associated Open Source technologies and successfully helped our customers adopt them.
+            Trying to reach out for the unachievable and what lies beyond us has brought us where we are today. Be it our first Linux cluster for SAP deployment in 2002, or our first deployment for Red Hat's integration suite Fuse in 2012 or for that matter our first Openstack deployment in 2015.
             </p> 
             <p className="mt-8">
-            We believe our strength lies in taking challenges head on and be of help to our customers in all areas of software deployment - architecture and planning, deployment and support. 
+            We never knew beforehand any of those areas when we set out to deliver. What kept us going and ultimately delivering was a focus to be able to of help to our customers and complete what we had started. 
             </p> 
             <p className="my-8">
-            Being a services focused organisation, we are able to think beyond product and strive to provide our customers with solutions that help them address their business need rather than deploy a product.   
-            </p>
-            <p className="mt-8">
-            We have been quite fortunate to have been able to work both in community and enterprise Open Source solution deployments. We have been living our dream of  creating people who are deft in fulfilling customer needs through Open Source means.
+            That same zest continues today when we pick up today's challenges of low code-no code, AI, Optaplanner etc. And we hope we are able to always aim for goals outside our reach with the will to achieve what we aim for.   
             </p>
              </div>
       </div>
